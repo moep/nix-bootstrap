41cd6e49b020cbc6c5b64f75f3c339c9069fa521
Karsten Groll <moep@users.noreply.github.com>
2020-07-07 22:55:15 +0200

    Using shellscripts as lib instead of submodule

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,29 +1,10 @@
 #!/usr/bin/env bash
 # Debug only
 #echo "SHELLSCRIPTS_HOME: ${SHELLSCRIPTS_HOME}"
-
-# Set shellscripts library home to ./shellscripts ...
-if [[ -z "${SHELLSCRIPTS_HOME}" ]]; then
-  #echo "SHELLSCRIPTS_HOME not set"
-  SHELLSCRIPTS_HOME=$(dirname "${BASH_SOURCE[0]}")"/shellscripts"
-  #echo " --> ${SHELLSCRIPTS_HOME}"
-fi
-
-# ... and clone it if it does not exist
-if [[ ! -d "${SHELLSCRIPTS_HOME}" ]]; then
-  echo "Cloning shellscripts library to ${SHELLSCRIPTS_HOME}..."
-  git clone https://github.com/moep/shellscripts.git "${SHELLSCRIPTS_HOME}"
-
-  if [[ $? != 0 ]]; then
-    echo "Something went wrong. Exiting."
-    exit 1
-  fi
-fi
-
-source "${SHELLSCRIPTS_HOME}/shellscriptloader-0.2/loader.sh"
+source "${BASH_SCRIPT_PATH}/shellscriptloader-0.2/loader.sh"
 
 loader_addpath "$(dirname "${BASH_SOURCE[0]}")" 
-loader_addpath "${SHELLSCRIPTS_HOME}"
+loader_addpath "${BASH_SCRIPT_PATH}"
 
 function bootstrap::finish() {
   loader_finish
