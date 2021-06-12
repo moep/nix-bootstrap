d0f2a0eec40d71bc92f41d8c52be3d0c0db5f10e
Karsten Groll <moep@users.noreply.github.com>
2021-06-13 00:24:37 +0200

    Added support for Ubuntu

--- src/dotfiles/linux.sh
+++ src/dotfiles/linux.sh
@@ -27,22 +27,43 @@ function dotfiles::_install_gentoo_prerequisites() {
 
 }
 
+function dotfiles::_install_ubuntu_prerequisites() {
+  local basic_pgms=(fish fzf git htop ncdu neovim ranger ripgrep tmux)
+  local sudo_cmd="sudo"
+
+  echo " [*] sudo apt update"
+  sudo apt update
+
+  echo " [*] sudo apt install ${basic_pgms[@]}"
+  sudo apt install "${basic_pgms[@]}"
+}
+
 function dotfiles::install_prerequisites() {
   ui::h2 "Running linux tasks"
   dotfiles::_create_pgp_keypair 
   dotfiles::_create_git_config
 
   # Distribution-dependent tasks
-  local distribution=$(cat /etc/*-release | grep "^NAME=" | cut -d '=' -f2)
+  local distribution=$(cat /etc/*-release | grep "^NAME=" | cut -d '=' -f2 | tr -d '"')
   ui::h2 "Running tasks for distribution ${distribution}"
 
   case "${distribution}" in
     "Gentoo")
       dotfiles::_install_gentoo_prerequisites
       ;;
+    "Ubuntu")
+      dotfiles::_install_ubuntu_prerequisites
+      ;;
     *)
-      echo "Unsupported distribution. Will not install anything"
+      echo "Unsupported distribution. Will not install anything."
+      ;;
   esac
+  
+  ui::h2 "Creating user ports"
+
+  echo -n "    delta"
+  os::exec_and_wait dotfiles::_install_delta
+  echo
 }
 
 function dotfiles::_create_pgp_keypair() {
@@ -60,4 +81,14 @@ function dotfiles::_create_git_config() {
   # git config --global user.email ...
 }
 
+function dotfiles::_install_delta() {
+  local tmp_dir=$(mktemp -d) 
+  cd ${tmp_dir} || return $RC_FAIL
+  curl -LO https://github.com/dandavison/delta/releases/download/0.8.0/delta-0.8.0-x86_64-unknown-linux-gnu.tar.gz || return $RC_FAIL
+  tar xvzf delta-0.8.0-x86_64-unknown-linux-gnu.tar.gz
 
+  mkdir -p ~/.local/bin > /dev/null
+  cp delta-0.8.0-x86_64-unknown-linux-gnu/delta ~/.local/bin
+
+  return $?
+}
